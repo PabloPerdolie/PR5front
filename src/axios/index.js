437d22bb4c21df2dbbbbfdd0881a36dde857c8c7
Pablo <parusov.pashka@ya.ru>
2023-10-02 01:18:23 +0300

    small fixes

--- src/axios/index.js
+++ src/axios/index.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const instance = axios.create({
     //baseURL: "http://localhost:3001",
-    baseURL: "https://PR5front.onrender.com"
+    baseURL: "https://PR5back.onrender.com"
 
 })
 
